1ecbac35292dfe900807280c93bebf38f0ee15fe
Clayton Pereira <clayton.pereira@outlook.com>
2020-09-29 17:13:23 -0300

    H2 DataBase

--- backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResource.java
@@ -2,32 +2,40 @@
  * Recursos(Conceito) que serão disponibilizados para as aplicações no Front-End
  * consumir, que por sua vez, é diponibilizada pela API do BackEnd.
  * Classe para responder as requisições REST (Recurso REST da "Entidade Category").
+ * * @Autowired trata a injeção/instânciação de dependência do Objeto anotado.
  */
 
 package com.devsuperior.dscatalog.resourses;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories" ) /* rota dos endpoints */
 public class CategoryResource {
 	
-	/* endpoints deste recurso */
+	//dependencias
+	@Autowired
+	private CategoryService service;
+	
+	/* endpoints (CRUD) deste recurso */
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
+		
+		List<Category> list = service.findAll();
+		
+		/*List<Category> list = new ArrayList<>();
 		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		list.add(new Category(2L, "Electronics"));*/ //lista mocada
 		
 		return ResponseEntity.ok().body(list);		
 	}
-	
 }
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,28 @@
+/* Pacote que disponibiliza a Camada de Serviço que Gerencia os
+ * Resources e Repositories da Entidade Category").
+ * @Service/@Component registra esta classe como um componente que participará
+ * do sistema de injeção de dependêcia automatizado do Spring".
+ * @Autowired trata a injeção/instânciação de dependência do Objeto anotado.
+ */
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	//dependecias
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+}
