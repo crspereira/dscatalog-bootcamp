48414b14b1d60b03d4a9863b508b75b739d7ce2f
Clayton Pereira <clayton.pereira@outlook.com>
2020-10-01 20:09:12 -0300

    Created Entity Product and Script SQLImport

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -1,30 +1,57 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
-import java.util.List;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	//instâncias de classse - TEM
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
 	private String imgUrl;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
 	//dependencias
-	private List<Category> categories;
+	/* Relação Muitos-Para-Muitos o vai garantir que um mesmo produto não terá
+	 * mais de uma categoria. O para da 3 tabela não se repita*/
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+			   joinColumns = @JoinColumn(name = "product_id"),
+			   inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
 	//construtores
 	public Product() {
 	}
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
+		this.date = date;
 	}
 	
 	//getters e setters
@@ -58,7 +85,13 @@ public class Product implements Serializable {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	public List<Category> getCategories() {
+	public Instant getDate() {
+		return date;
+	}
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+	public Set<Category> getCategories() {
 		return categories;
 	}
 	
