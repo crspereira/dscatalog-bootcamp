4fdb170b018ab7b34c2c7b17c64670cbb44e1765
Clayton Pereira <clayton.pereira@outlook.com>
2020-10-06 10:59:55 -0300

    Clean Project Structure

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//notação function
+/* function App(){
+  return <h1>Now let's learning React!</h1>;
+} */
+
+//notação arrow function
+const App = () => {
+    return <h1>Hello World! Now let's learning React!</h1>;
+  }
 
 export default App;
