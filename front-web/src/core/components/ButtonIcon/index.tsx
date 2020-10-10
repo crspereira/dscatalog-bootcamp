295ed657c269cfa18e48b03fe4cb6eb3b24cfe3f
Clayton Pereira <clayton.pereira@outlook.com>
2020-10-10 19:36:24 -0300

    Adding Home Page

---
+++ front-web/src/core/components/ButtonIcon/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg';
+import './styles.scss';
+
+type Props = {
+   textButton: string;
+}
+
+const ButtonIcon = ({textButton}: Props) => {
+   return (
+      <div className="d-flex">
+         <button className="btn btn-primary btn-icon">
+            <h5>{textButton}</h5>
+         </button>
+         <div className="btn-icon-content">
+            <ArrowIcon />
+         </div>
+      </div>
+   );
+}
+
+export default ButtonIcon;
