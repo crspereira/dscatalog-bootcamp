63080b525382ca55c1e61e3dfb55d4969c5f94f7
Clayton Pereira <clayton.pereira@outlook.com>
2020-11-01 12:32:43 -0300

    BackEnd Cap-4 - Added OAuth DBH2 Configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,9 +19,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	//dependencias
 	@Autowired
 	private JwtTokenStore tokenStore;
+	@Autowired //libera o banco H2
+	private Environment environment;
 	
 		//contantes de permissionamento
-		private static final String[] PUBLIC = { "/oauth/token" };
+		private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
 		private static final String[] OPERATOR_OR_ADMIN = { "/products/**",  "/categories/**"};
 		private static final String[] ADMIN = { "/users/**" };
 	
@@ -30,6 +35,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//libera o Banco H2
+		if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
