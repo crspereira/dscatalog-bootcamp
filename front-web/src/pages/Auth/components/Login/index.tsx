5e136c64fde6b7a9c637664f25aa32e2bb7f96d4
Clayton Pereira <clayton.pereira@outlook.com>
2020-11-02 15:31:22 -0300

    FrontEnd Cap-5 - Added Login Form - Using ReactHook

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,30 +1,52 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
 import './styles.scss';
 
+//modelo de dados a ser enviado pelo ReactHookForm
+type FormData = {
+   email: string;
+   password: string;
+}
+
 const Login = () => {
+   //iniciando o ReactHook Form
+   const { register, handleSubmit } = useForm<FormData>();
+   const onSubmit = (data: FormData) => {
+      return (
+         console.log(data) //data pode ser qualquer nome
+         // chamar API de autenticação
+      );
+   }
+
    return (
       <div className="login-container" >
          <AuthCard title="Login">
-            <form action="" className="login-form">
+            <form onSubmit={handleSubmit(onSubmit)} className="login-form">
                <input 
-                  type="email"
+                  type="email" //type é do html5
                   className="form-control input-base margin-button-30" //form-control é do bootstrap
                   placeholder="Email"
+                  //integração com o ReactHook Form
+                  name="email"
+                  ref={register}
                />
                <input 
-                  type="password"
+                  type="password" //type é do html5
                   className="form-control input-base" //form-control é do bootstrap
                   placeholder="Senha"
+                  //integração com o ReactHook Form
+                  name="password"
+                  ref={register}
                />
                <Link to="/admin/auth/recover" className="login-link-recover" >
                   Esqueci a senha?
                </Link>
                <div className="login-submit">
+                  <ButtonIcon textButton="Logar"/>
                   <Link to="/admin/products/create" className="login-link-recover" >
-                     <ButtonIcon textButton="Logar"/>
                   </Link>
                </div>
                <div className="not-registered-content">
