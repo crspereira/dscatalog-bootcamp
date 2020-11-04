de4f643b0bfbd2b9339d0be264c8c5b2cb4d30c3
Clayton Pereira <clayton.pereira@outlook.com>
2020-11-04 14:49:16 -0300

    FrontEnd Cap-5 - Added Fields Login Authentication

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,6 +1,6 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import { makeLogin } from 'core/utils/request';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import AuthCard from '../Card';
@@ -15,36 +15,51 @@ type FormData = {
 const Login = () => {
    //iniciando o ReactHook Form
    const { register, handleSubmit } = useForm<FormData>();
+   const [hasError, setHasError] = useState(false);
+
    const onSubmit = (data: FormData) => { //variável data pode ser qualquer nome
-      console.log(data);
-      makeLogin(data); // chamar API de autenticação
+      //console.log(data);
+      makeLogin(data) // chamar API de autenticação
+         .then( response => {
+            setHasError(false);
+         })
+         .catch(() => {
+            setHasError(true);
+         })
    }
+   
 
    return (
       <div className="login-container" >
          <AuthCard title="Login">
+            {/* Valida Login */}
+            {hasError && (
+               <div className="alert alert-danger mt-5" >
+               Usuário ou Senha inválido!
+               </div>
+            )}
             <form onSubmit={handleSubmit(onSubmit)} className="login-form">
-               <input 
+               <input
                   type="email" //type é do html5
                   className="form-control input-base margin-button-30" //form-control é do bootstrap
                   placeholder="Email"
                   //integração com o ReactHook Form
                   name="username"
-                  ref={register}
+                  ref={register({ required: true })}
                />
-               <input 
+               <input
                   type="password" //type é do html5
                   className="form-control input-base" //form-control é do bootstrap
                   placeholder="Senha"
                   //integração com o ReactHook Form
                   name="password"
-                  ref={register}
+                  ref={register({ required: true })}
                />
                <Link to="/admin/auth/recover" className="login-link-recover" >
                   Esqueci a senha?
                </Link>
                <div className="login-submit">
-                  <ButtonIcon textButton="Logar"/>
+                  <ButtonIcon textButton="Logar" />
                </div>
                <div className="not-registered-content">
                   <span className="not-registered-content-text">
