7f1aa5fb728c5a9ecc33a071524fa5c7b88479d0
Clayton Pereira <clayton.pereira@outlook.com>
2021-01-05 12:42:09 -0300

    FrontEnd Cap-5 - Added Validation and Tostfy Alert on NewProdutcs Form

--- front-web/src/pages/Admin/components/ProductsAdmin/ProductForm/index.tsx
+++ front-web/src/pages/Admin/components/ProductsAdmin/ProductForm/index.tsx
@@ -1,6 +1,8 @@
 import { makePrivateRequest } from 'core/utils/request';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import BaseFormAdmin from '../../BaseFormAdmin';
 import './styles.scss';
 
@@ -12,11 +14,19 @@ type FormState = {
 }
 
 const ProductForm = () => {
-   const { register, handleSubmit } = useForm<FormState>();
+   const { register, handleSubmit, errors } = useForm<FormState>();
+   const history = useHistory();
  
    const onSubmit = (data: FormState) => {
-      //console.log(data);
-      makePrivateRequest({ url:'/products', method:'POST', data });
+      console.log(data);
+      makePrivateRequest({ url:'/products', method:'POST', data })
+         .then(() => {
+            toast.info('Produto Salvo com Sucesso!');
+            history.push('/admin/products');
+         })
+         .catch(() => {
+            toast.error('Erro ao Salvar o Produto!');
+         })
    }
 
    return (
@@ -24,13 +34,25 @@ const ProductForm = () => {
          <BaseFormAdmin title="Cadastrar um Produto">
             <div className="row mt-4">
                <div className="col-6">
-                  <input
-                     ref={register({ required: "Campo Obrigatório" })}
-                     name="name"
-                     type="text"
-                     className="form-control margin-botton-30 input-base"
-                     placeholder="Nome do Produto"
-                  />
+                  <div className="margin-botton-30">
+                     <input
+                        ref={register({
+                           required: "Campo Obrigatório",
+                           minLength: { value: 5, message: 'O Campos deve ter no Mínimo 5 Caracteres'},
+                           maxLength: { value: 60, message: 'O Campos deve ter no Máximo 60 Caracteres'}
+                        })}
+                        name="name"
+                        type="text"
+                        className="form-control input-base"
+                        placeholder="Nome do Produto"
+                     />
+                     {errors.name && (
+                        <div className="invalid-feedback d-block">
+                           {errors.name.message}
+                        </div>
+                     )}
+                  </div>
+                  
                   {/* <select
                      value={formData.category}
                      name="category"
@@ -41,20 +63,34 @@ const ProductForm = () => {
                      <option value="2">Eletrônicos</option>
                      <option value="3">Computadores</option>
                   </select> */}
-                  <input
-                     ref={register({ required: "Campo Obrigatório" })}
-                     name="price"
-                     type="number"
-                     className="form-control margin-botton-30 input-base"
-                     placeholder="Preço"
-                  />
-                  <input
-                     ref={register({ required: "Campo Obrigatório" })}
-                     name="imgUrl"
-                     type="text"
-                     className="form-control margin-botton-30 input-base"
-                     placeholder="URL da Imagem"
-                  />
+                  <div className="margin-botton-30">
+                     <input
+                        ref={register({ required: "Campo Obrigatório" })}
+                        name="price"
+                        type="number"
+                        className="form-control input-base"
+                        placeholder="Preço"
+                     />
+                     {errors.price && (
+                        <div className="invalid-feedback d-block">
+                           {errors.price.message}
+                        </div>
+                     )}
+                  </div>
+                  <div className="margin-botton-30">
+                     <input
+                        ref={register({ required: "Campo Obrigatório" })}
+                        name="imageUrl"
+                        type="text"
+                        className="form-control input-base"
+                        placeholder="URL da Imagem"
+                     />
+                     {errors.imageUrl && (
+                        <div className="invalid-feedback d-block">
+                           {errors.imageUrl.message}
+                        </div>
+                     )}
+                  </div>
                </div>
                <div className="col-6">
                   <textarea
@@ -65,6 +101,11 @@ const ProductForm = () => {
                      className="form-control input-base"
                      placeholder="Descrição"
                   />
+                  {errors.description && (
+                     <div className="invalid-feedback d-block">
+                        {errors.description.message}
+                     </div>
+                  )}
                </div>
             </div>
          </BaseFormAdmin>
